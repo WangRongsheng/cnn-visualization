37ee76f2a74a1ac9dd942b42bf839f410d2a2773
coder-王荣胜 <fujingshusheng@gmail.com>
2021-02-25 11:21:03 +0800

    init project

---
+++ CNN卷积可视化实现/MainForm.Designer.cs
@@ -0,0 +1,399 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 2021/2/15
+ * Time: 18:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace CNN卷积可视化实现
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonScan = new System.Windows.Forms.Button();
+			this.buttonUp = new System.Windows.Forms.Button();
+			this.buttonNext = new System.Windows.Forms.Button();
+			this.buttonBegin = new System.Windows.Forms.Button();
+			this.pictureBoxShow = new System.Windows.Forms.PictureBox();
+			this.textBoxPicturePath = new System.Windows.Forms.TextBox();
+			this.labelPicturePath = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBoxRed = new System.Windows.Forms.PictureBox();
+			this.pictureBoxGreen = new System.Windows.Forms.PictureBox();
+			this.pictureBoxBlue = new System.Windows.Forms.PictureBox();
+			this.pictureBoxJuanjiRed = new System.Windows.Forms.PictureBox();
+			this.pictureBoxJuanjiGreen = new System.Windows.Forms.PictureBox();
+			this.pictureBoxJuanjiBlue = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBoxJuanjiHe = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBoxbuchang = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.pictureBoxJuanjiHebing = new System.Windows.Forms.PictureBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.pictureBoxJili = new System.Windows.Forms.PictureBox();
+			this.pictureBoxChihua = new System.Windows.Forms.PictureBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.comboBoxjili = new System.Windows.Forms.ComboBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxShow)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxJuanjiRed)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxJuanjiGreen)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxJuanjiBlue)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxJuanjiHebing)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxJili)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxChihua)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// buttonScan
+			// 
+			this.buttonScan.Font = new System.Drawing.Font("宋体", 9F);
+			this.buttonScan.Location = new System.Drawing.Point(34, 568);
+			this.buttonScan.Name = "buttonScan";
+			this.buttonScan.Size = new System.Drawing.Size(75, 25);
+			this.buttonScan.TabIndex = 0;
+			this.buttonScan.Text = "浏览";
+			this.buttonScan.UseVisualStyleBackColor = true;
+			this.buttonScan.Click += new System.EventHandler(this.ButtonScanClick);
+			// 
+			// buttonUp
+			// 
+			this.buttonUp.Font = new System.Drawing.Font("宋体", 9F);
+			this.buttonUp.Location = new System.Drawing.Point(34, 614);
+			this.buttonUp.Name = "buttonUp";
+			this.buttonUp.Size = new System.Drawing.Size(75, 25);
+			this.buttonUp.TabIndex = 1;
+			this.buttonUp.Text = "上一张";
+			this.buttonUp.UseVisualStyleBackColor = true;
+			this.buttonUp.Click += new System.EventHandler(this.ButtonUpClick);
+			// 
+			// buttonNext
+			// 
+			this.buttonNext.Font = new System.Drawing.Font("宋体", 9F);
+			this.buttonNext.Location = new System.Drawing.Point(142, 614);
+			this.buttonNext.Name = "buttonNext";
+			this.buttonNext.Size = new System.Drawing.Size(75, 25);
+			this.buttonNext.TabIndex = 1;
+			this.buttonNext.Text = "下一张";
+			this.buttonNext.UseVisualStyleBackColor = true;
+			this.buttonNext.Click += new System.EventHandler(this.ButtonNextClick);
+			// 
+			// buttonBegin
+			// 
+			this.buttonBegin.Font = new System.Drawing.Font("宋体", 9F);
+			this.buttonBegin.Location = new System.Drawing.Point(142, 568);
+			this.buttonBegin.Name = "buttonBegin";
+			this.buttonBegin.Size = new System.Drawing.Size(75, 25);
+			this.buttonBegin.TabIndex = 0;
+			this.buttonBegin.Text = "开始";
+			this.buttonBegin.UseVisualStyleBackColor = true;
+			this.buttonBegin.Click += new System.EventHandler(this.ButtonBeginClick);
+			// 
+			// pictureBoxShow
+			// 
+			this.pictureBoxShow.Location = new System.Drawing.Point(18, 301);
+			this.pictureBoxShow.Name = "pictureBoxShow";
+			this.pictureBoxShow.Size = new System.Drawing.Size(240, 240);
+			this.pictureBoxShow.TabIndex = 2;
+			this.pictureBoxShow.TabStop = false;
+			// 
+			// textBoxPicturePath
+			// 
+			this.textBoxPicturePath.Location = new System.Drawing.Point(18, 80);
+			this.textBoxPicturePath.Name = "textBoxPicturePath";
+			this.textBoxPicturePath.Size = new System.Drawing.Size(240, 25);
+			this.textBoxPicturePath.TabIndex = 3;
+			// 
+			// labelPicturePath
+			// 
+			this.labelPicturePath.Font = new System.Drawing.Font("宋体", 9F);
+			this.labelPicturePath.Location = new System.Drawing.Point(18, 44);
+			this.labelPicturePath.Name = "labelPicturePath";
+			this.labelPicturePath.Size = new System.Drawing.Size(240, 33);
+			this.labelPicturePath.TabIndex = 4;
+			this.labelPicturePath.Text = "图片路径：";
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("宋体", 12F);
+			this.label1.Location = new System.Drawing.Point(91, 19);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(114, 39);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "输入区";
+			// 
+			// pictureBoxRed
+			// 
+			this.pictureBoxRed.Location = new System.Drawing.Point(356, 40);
+			this.pictureBoxRed.Name = "pictureBoxRed";
+			this.pictureBoxRed.Size = new System.Drawing.Size(240, 240);
+			this.pictureBoxRed.TabIndex = 6;
+			this.pictureBoxRed.TabStop = false;
+			// 
+			// pictureBoxGreen
+			// 
+			this.pictureBoxGreen.Location = new System.Drawing.Point(356, 299);
+			this.pictureBoxGreen.Name = "pictureBoxGreen";
+			this.pictureBoxGreen.Size = new System.Drawing.Size(240, 240);
+			this.pictureBoxGreen.TabIndex = 6;
+			this.pictureBoxGreen.TabStop = false;
+			// 
+			// pictureBoxBlue
+			// 
+			this.pictureBoxBlue.Location = new System.Drawing.Point(356, 562);
+			this.pictureBoxBlue.Name = "pictureBoxBlue";
+			this.pictureBoxBlue.Size = new System.Drawing.Size(240, 240);
+			this.pictureBoxBlue.TabIndex = 6;
+			this.pictureBoxBlue.TabStop = false;
+			// 
+			// pictureBoxJuanjiRed
+			// 
+			this.pictureBoxJuanjiRed.Location = new System.Drawing.Point(665, 40);
+			this.pictureBoxJuanjiRed.Name = "pictureBoxJuanjiRed";
+			this.pictureBoxJuanjiRed.Size = new System.Drawing.Size(240, 240);
+			this.pictureBoxJuanjiRed.TabIndex = 6;
+			this.pictureBoxJuanjiRed.TabStop = false;
+			// 
+			// pictureBoxJuanjiGreen
+			// 
+			this.pictureBoxJuanjiGreen.Location = new System.Drawing.Point(665, 299);
+			this.pictureBoxJuanjiGreen.Name = "pictureBoxJuanjiGreen";
+			this.pictureBoxJuanjiGreen.Size = new System.Drawing.Size(240, 240);
+			this.pictureBoxJuanjiGreen.TabIndex = 6;
+			this.pictureBoxJuanjiGreen.TabStop = false;
+			// 
+			// pictureBoxJuanjiBlue
+			// 
+			this.pictureBoxJuanjiBlue.Location = new System.Drawing.Point(665, 562);
+			this.pictureBoxJuanjiBlue.Name = "pictureBoxJuanjiBlue";
+			this.pictureBoxJuanjiBlue.Size = new System.Drawing.Size(240, 240);
+			this.pictureBoxJuanjiBlue.TabIndex = 6;
+			this.pictureBoxJuanjiBlue.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("宋体", 12F);
+			this.label2.Location = new System.Drawing.Point(414, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(114, 28);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "RGB通道";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("宋体", 12F);
+			this.label3.Location = new System.Drawing.Point(727, 9);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(114, 28);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "卷积层";
+			// 
+			// textBoxJuanjiHe
+			// 
+			this.textBoxJuanjiHe.Location = new System.Drawing.Point(18, 152);
+			this.textBoxJuanjiHe.Name = "textBoxJuanjiHe";
+			this.textBoxJuanjiHe.Size = new System.Drawing.Size(240, 25);
+			this.textBoxJuanjiHe.TabIndex = 3;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("宋体", 9F);
+			this.label4.Location = new System.Drawing.Point(18, 116);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(240, 33);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "卷积核：";
+			// 
+			// textBoxbuchang
+			// 
+			this.textBoxbuchang.Location = new System.Drawing.Point(129, 207);
+			this.textBoxbuchang.Name = "textBoxbuchang";
+			this.textBoxbuchang.Size = new System.Drawing.Size(129, 25);
+			this.textBoxbuchang.TabIndex = 3;
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("宋体", 9F);
+			this.label5.Location = new System.Drawing.Point(18, 210);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(69, 33);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "步长：";
+			// 
+			// pictureBoxJuanjiHebing
+			// 
+			this.pictureBoxJuanjiHebing.Location = new System.Drawing.Point(962, 40);
+			this.pictureBoxJuanjiHebing.Name = "pictureBoxJuanjiHebing";
+			this.pictureBoxJuanjiHebing.Size = new System.Drawing.Size(240, 240);
+			this.pictureBoxJuanjiHebing.TabIndex = 6;
+			this.pictureBoxJuanjiHebing.TabStop = false;
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("宋体", 12F);
+			this.label6.Location = new System.Drawing.Point(1006, 9);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(164, 28);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "卷积层输出合并";
+			// 
+			// pictureBoxJili
+			// 
+			this.pictureBoxJili.Location = new System.Drawing.Point(962, 301);
+			this.pictureBoxJili.Name = "pictureBoxJili";
+			this.pictureBoxJili.Size = new System.Drawing.Size(240, 240);
+			this.pictureBoxJili.TabIndex = 6;
+			this.pictureBoxJili.TabStop = false;
+			// 
+			// pictureBoxChihua
+			// 
+			this.pictureBoxChihua.Location = new System.Drawing.Point(962, 564);
+			this.pictureBoxChihua.Name = "pictureBoxChihua";
+			this.pictureBoxChihua.Size = new System.Drawing.Size(240, 240);
+			this.pictureBoxChihua.TabIndex = 6;
+			this.pictureBoxChihua.TabStop = false;
+			// 
+			// label7
+			// 
+			this.label7.Font = new System.Drawing.Font("宋体", 12F);
+			this.label7.Location = new System.Drawing.Point(1220, 260);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(37, 106);
+			this.label7.TabIndex = 7;
+			this.label7.Text = "激励层";
+			// 
+			// label8
+			// 
+			this.label8.Font = new System.Drawing.Font("宋体", 12F);
+			this.label8.Location = new System.Drawing.Point(1220, 514);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(37, 106);
+			this.label8.TabIndex = 7;
+			this.label8.Text = "池化层";
+			// 
+			// label9
+			// 
+			this.label9.Font = new System.Drawing.Font("宋体", 9F);
+			this.label9.Location = new System.Drawing.Point(18, 256);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(91, 33);
+			this.label9.TabIndex = 4;
+			this.label9.Text = "激励函数：";
+			// 
+			// comboBoxjili
+			// 
+			this.comboBoxjili.FormattingEnabled = true;
+			this.comboBoxjili.Location = new System.Drawing.Point(129, 253);
+			this.comboBoxjili.Name = "comboBoxjili";
+			this.comboBoxjili.Size = new System.Drawing.Size(129, 23);
+			this.comboBoxjili.TabIndex = 8;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1302, 814);
+			this.Controls.Add(this.comboBoxjili);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.pictureBoxChihua);
+			this.Controls.Add(this.pictureBoxJuanjiBlue);
+			this.Controls.Add(this.pictureBoxBlue);
+			this.Controls.Add(this.pictureBoxJili);
+			this.Controls.Add(this.pictureBoxJuanjiGreen);
+			this.Controls.Add(this.pictureBoxJuanjiHebing);
+			this.Controls.Add(this.pictureBoxJuanjiRed);
+			this.Controls.Add(this.pictureBoxGreen);
+			this.Controls.Add(this.pictureBoxRed);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBoxbuchang);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBoxJuanjiHe);
+			this.Controls.Add(this.labelPicturePath);
+			this.Controls.Add(this.textBoxPicturePath);
+			this.Controls.Add(this.pictureBoxShow);
+			this.Controls.Add(this.buttonNext);
+			this.Controls.Add(this.buttonUp);
+			this.Controls.Add(this.buttonBegin);
+			this.Controls.Add(this.buttonScan);
+			this.Name = "MainForm";
+			this.Text = "CNN卷积可视化实现";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxShow)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxJuanjiRed)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxJuanjiGreen)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxJuanjiBlue)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxJuanjiHebing)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxJili)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxChihua)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.ComboBox comboBoxjili;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.PictureBox pictureBoxChihua;
+		private System.Windows.Forms.PictureBox pictureBoxJili;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.PictureBox pictureBoxJuanjiHebing;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBoxbuchang;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBoxJuanjiHe;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBoxJuanjiBlue;
+		private System.Windows.Forms.PictureBox pictureBoxJuanjiGreen;
+		private System.Windows.Forms.PictureBox pictureBoxJuanjiRed;
+		private System.Windows.Forms.PictureBox pictureBoxBlue;
+		private System.Windows.Forms.PictureBox pictureBoxGreen;
+		private System.Windows.Forms.PictureBox pictureBoxRed;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label labelPicturePath;
+		private System.Windows.Forms.TextBox textBoxPicturePath;
+		private System.Windows.Forms.PictureBox pictureBoxShow;
+		private System.Windows.Forms.Button buttonBegin;
+		private System.Windows.Forms.Button buttonNext;
+		private System.Windows.Forms.Button buttonUp;
+		private System.Windows.Forms.Button buttonScan;
+	}
+}
---
+++ CNN卷积可视化实现/MainForm.cs
@@ -0,0 +1,319 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 2021/2/15
+ * Time: 18:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+namespace CNN卷积可视化实现
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		string[] path;
+		int count=0;
+		int height;
+		int width;
+		int [,] colorRed,colorGreen,colorBlue;
+		int [,] juanjicolorRed,juanjicolorGreen,juanjicolorBlue;
+		int [,] juanjihe=new int[3,3]{{1,0,-1},{1,0,-1},{1,0,-1}};
+		int [] W,B;
+		double r=0.02;
+		int juanjibuchang=1;
+		int juanjiwidth=3;
+		int [,] colorchihuaRed,colorchihuaGreen,colorchihuaBlue;
+		Bitmap pictureRed,pictureGreen,pictureBlue;
+		Bitmap juanjipictureRed,juanjipictureGreen,juanjipictureBlue,juanjihebingpicture;
+		Bitmap jilipicture;
+		Bitmap chihuapicture;
+		public MainForm()
+		{
+			InitializeComponent();
+			comboBoxjili.Items.Add("Relu函数");
+			comboBoxjili.Items.Add("Sigmoid函数");
+			comboBoxjili.Items.Add("tanh函数");
+			comboBoxjili.Items.Add("ELU函数");
+		}
+		
+		void ButtonScanClick(object sender, EventArgs e)
+		{
+			height=240;
+			width=240;
+			try{
+				path = Directory.GetFiles(@textBoxPicturePath.Text.ToString(), "*.jpg");
+				PictureShow();
+			}
+			catch{
+				MessageBox.Show("图片路径错误,重新输入");
+			}
+			
+		}
+		void PictureShow()
+		{
+			Bitmap b=new Bitmap(path[count]);
+			b=b.Clone(new Rectangle(0,0,240,240),System.Drawing.Imaging.PixelFormat.DontCare);
+			pictureBoxShow.Image=Image.FromHbitmap(b.GetHbitmap());
+			GetRGB(b);
+		}
+		
+		void ButtonUpClick(object sender, EventArgs e)
+		{
+			height=240;
+			width=240;
+			if(count>0&&count<path.Length)
+			{	count--;
+				PictureShow();
+			}
+			else
+			{
+				MessageBox.Show("这已经是第一张了");
+				
+			}
+		}
+		void GetRGB(Bitmap b)
+		{
+			colorRed=new int[height,width];
+			colorGreen=new int[height,width];
+			colorBlue = new int[height,width];
+			Rectangle rect = new Rectangle(0,0,width,height);
+			
+			System.Drawing.Imaging.BitmapData bmpData =b.LockBits(rect,System.Drawing.Imaging.ImageLockMode.ReadWrite,b.PixelFormat);
+			IntPtr iptr = bmpData.Scan0;
+			int iBytes = width*height*3;
+			byte[] PixelValues = new byte[iBytes];
+			System.Runtime.InteropServices.Marshal.Copy(iptr,PixelValues,0,iBytes);
+			
+			b.UnlockBits(bmpData);
+			pictureRed =new Bitmap(width,height);
+			pictureGreen=new Bitmap(width,height);
+			pictureBlue= new Bitmap(width,height);
+			int ipoint=0;
+			for(int i=0;i<width;++i)
+			{
+				for(int j=0;j<height;++j)
+				{
+					
+					colorRed[i,j] = Convert.ToInt32(PixelValues[ipoint++]);
+					pictureRed.SetPixel(j,i,Color.FromArgb(colorRed[i,j],0,0));
+					colorGreen[i,j] = Convert.ToInt32(PixelValues[ipoint++]);
+					pictureGreen.SetPixel(j,i,Color.FromArgb(0,colorGreen[i,j],0));
+					colorBlue[i,j] = Convert.ToInt32(PixelValues[ipoint++]);
+					pictureBlue.SetPixel(j,i,Color.FromArgb(0,0,colorBlue[i,j]));
+					
+				}
+			}
+			
+		}
+		
+		void ButtonNextClick(object sender, EventArgs e)
+		{
+			height=240;
+			width=240;	
+			if(count<path.Length-1)
+			{   count++;
+				PictureShow();
+			}
+			else
+			{
+				MessageBox.Show("这已经是最后一张了");
+			}
+		}
+		
+		void ButtonBeginClick(object sender, EventArgs e)
+		{
+			height=240;
+			width=240;
+			if(textBoxJuanjiHe.Text!="")
+			{
+				string[] data=textBoxJuanjiHe.Text.ToString().Split(',');
+				if(data.Length!=Math.Sqrt(data.Length)*Math.Sqrt(data.Length))
+					MessageBox.Show("卷积核格式错误");
+				else{
+					juanjihe=new int[(int)Math.Sqrt(data.Length),(int)Math.Sqrt(data.Length)];
+					for(int i=0;i<data.Length;++i)
+						juanjihe[i/(int)Math.Sqrt(data.Length),i%(int)Math.Sqrt(data.Length)]=Convert.ToInt32(data[i]);
+					juanjiwidth=(int)Math.Sqrt(data.Length);
+				}
+			}
+			if(textBoxbuchang.Text!="")
+				juanjibuchang=int.Parse(textBoxbuchang.Text.ToString());
+			juanjicolorRed=new int[(width-juanjiwidth+1)/juanjibuchang,(height-juanjiwidth+1)/juanjibuchang];
+			juanjicolorGreen=new int[(width-juanjiwidth+1)/juanjibuchang,(height-juanjiwidth+1)/juanjibuchang];
+			juanjicolorBlue=new int[(width-juanjiwidth+1)/juanjibuchang,(height-juanjiwidth+1)/juanjibuchang];
+			juanjipictureRed =new Bitmap((width-juanjiwidth+1)/juanjibuchang,(height-juanjiwidth+1)/juanjibuchang);
+			juanjipictureGreen=new Bitmap((width-juanjiwidth+1)/juanjibuchang,(height-juanjiwidth+1)/juanjibuchang);
+			juanjipictureBlue= new Bitmap((width-juanjiwidth+1)/juanjibuchang,(height-juanjiwidth+1)/juanjibuchang);
+			juanjihebingpicture=new Bitmap((width-juanjiwidth+1)/juanjibuchang,(height-juanjiwidth+1)/juanjibuchang);
+			//
+			//卷积层
+			//
+			juanji(colorRed,out juanjicolorRed);
+			juanji(colorGreen,out juanjicolorGreen);
+			juanji(colorBlue,out juanjicolorBlue);
+			width=(width-juanjiwidth+1)/juanjibuchang;
+			height=(height-juanjiwidth+1)/juanjibuchang;
+			pictureBoxRed.Image=Image.FromHbitmap(pictureRed.GetHbitmap());
+			pictureBoxGreen.Image=Image.FromHbitmap(pictureGreen.GetHbitmap());
+			pictureBoxBlue.Image=Image.FromHbitmap(pictureBlue.GetHbitmap());
+			for(int i=0;i<width;++i)
+				for(int j=0;j<height;++j)
+			{
+				juanjipictureRed.SetPixel(j,i,Color.FromArgb(Math.Abs(juanjicolorRed[i,j]%255),0,0));
+			}
+			pictureBoxJuanjiRed.Image=Image.FromHbitmap(juanjipictureRed.GetHbitmap());
+			for(int i=0;i<width;++i)
+				for(int j=0;j<height;++j)
+			{
+				juanjipictureGreen.SetPixel(j,i,Color.FromArgb(0,Math.Abs(juanjicolorGreen[i,j]%255),0));
+			}
+			pictureBoxJuanjiGreen.Image=Image.FromHbitmap(juanjipictureGreen.GetHbitmap());
+			for(int i=0;i<width;++i)
+				for(int j=0;j<height;++j)
+			{
+				juanjipictureBlue.SetPixel(j,i,Color.FromArgb(0,0,Math.Abs(juanjicolorBlue[i,j]%255)));
+			}
+			pictureBoxJuanjiBlue.Image=Image.FromHbitmap(juanjipictureBlue.GetHbitmap());
+			for(int i=0;i<width;++i)
+				for(int j=0;j<height;++j)
+			{
+				juanjihebingpicture.SetPixel(j,i,Color.FromArgb(Math.Abs(juanjicolorRed[i,j]%255),Math.Abs(juanjicolorGreen[i,j]%255),Math.Abs(juanjicolorBlue[i,j]%255)));
+			}
+			pictureBoxJuanjiHebing.Image=Image.FromHbitmap(juanjihebingpicture.GetHbitmap());
+			//
+			//激励层
+			//
+			Random random =new Random();
+			W=new int[height];
+			B=new int[height];
+			for(int i=0;i<height;++i)
+			{
+				W[i]=random.Next(0,2);
+				B[i]=random.Next(0,2);
+			}
+			for(int i=0;i<width;++i)
+				for(int j=0;j<height;++j)
+			{
+				juanjicolorRed[i,j]=juanjicolorRed[i,j]*W[j]+B[j];
+				juanjicolorGreen[i,j]=juanjicolorGreen[i,j]*W[j]+B[j];
+				juanjicolorBlue[i,j]=juanjicolorBlue[i,j]*W[j]+B[j];
+			}
+				
+			if(comboBoxjili.Text=="ELU函数")
+				ELU();
+			else if(comboBoxjili.Text=="Sigmoid函数")
+				sigmoid();
+			else if(comboBoxjili.Text=="tanh函数")
+				tanh();
+			else Relu();
+			jilipicture=new Bitmap(width,height);
+			for(int i=0;i<width;++i)
+				for(int j=0;j<height;++j)
+			{
+				jilipicture.SetPixel(j,i,Color.FromArgb(Math.Abs(juanjicolorRed[i,j]%255),Math.Abs(juanjicolorGreen[i,j]%255),Math.Abs(juanjicolorBlue[i,j]%255)));
+			}
+			pictureBoxJili.Image=Image.FromHbitmap(jilipicture.GetHbitmap());
+			
+			//
+			//池化层
+			//
+			colorchihuaRed=new int[width/2,height/2];
+			colorchihuaGreen=new int[width/2,height/2];
+			colorchihuaBlue=new int[width/2,height/2];
+			for(int i=0;i<width-1;i+=2)
+				for(int j=0;j<height-1;j+=2)
+			{
+				int maxred=juanjicolorRed[i,j];
+				int maxgreen=juanjicolorGreen[i,j];
+				int maxblue=juanjicolorBlue[i,j];
+				maxred=Math.Max(Math.Max(Math.Max(juanjicolorRed[i,j+1],juanjicolorRed[i+1,j]),juanjicolorRed[i+1,j+1]),maxred);
+				colorchihuaRed[i/2,j/2]=maxred;
+				maxgreen=Math.Max(Math.Max(Math.Max(juanjicolorGreen[i,j+1],juanjicolorGreen[i+1,j]),juanjicolorGreen[i+1,j+1]),maxgreen);
+				colorchihuaGreen[i/2,j/2]=maxgreen;
+				maxblue=Math.Max(Math.Max(Math.Max(juanjicolorBlue[i,j+1],juanjicolorBlue[i+1,j]),juanjicolorBlue[i+1,j+1]),maxblue);
+				colorchihuaBlue[i/2,j/2]=maxblue;
+			}
+			chihuapicture= new Bitmap(width/2,height/2);
+			for(int i=0;i<width/2;++i)
+				for(int j=0;j<height/2;++j)
+			{
+				chihuapicture.SetPixel(j,i,Color.FromArgb(Math.Abs(colorchihuaRed[i,j]%255),Math.Abs(colorchihuaGreen[i,j]%255),Math.Abs(colorchihuaBlue[i,j]%255)));
+			}
+			pictureBoxChihua.Image=Image.FromHbitmap(chihuapicture.GetHbitmap());
+		}
+		void Relu()
+		{
+			for(int i=0;i<width;++i)
+				for(int j=0;j<height;++j)
+			{
+				if(juanjicolorRed[i,j]<=0)
+					juanjicolorRed[i,j]=(int)r*juanjicolorRed[i,j];
+				juanjicolorGreen[i,j]=juanjicolorGreen[i,j]*W[j]+B[j];
+				if(juanjicolorGreen[i,j]<=0)
+					juanjicolorGreen[i,j]=(int)r*juanjicolorGreen[i,j];
+				juanjicolorBlue[i,j]=juanjicolorBlue[i,j]*W[j]+B[j];
+				if(juanjicolorBlue[i,j]<=0)
+					juanjicolorBlue[i,j]=(int)r*juanjicolorBlue[i,j];
+			}
+		}
+		void sigmoid(){
+			for(int i=0;i<width;++i)
+				for(int j=0;j<height;++j)
+			{
+				juanjicolorRed[i,j]=(int)(255/(1+Math.Exp(-juanjicolorRed[i,j])));
+				juanjicolorGreen[i,j]=(int)(255/(1+Math.Exp(-juanjicolorGreen[i,j])));
+				juanjicolorBlue[i,j]=(int)(255/(1+Math.Exp(-juanjicolorBlue[i,j])));
+			}
+		}
+		void tanh()
+		{
+			for(int i=0;i<width;++i)
+				for(int j=0;j<height;++j)
+			{
+				juanjicolorRed[i,j]=(int)((Math.Exp(juanjicolorRed[i,j])-Math.Exp(-juanjicolorRed[i,j]))/(Math.Exp(juanjicolorRed[i,j])+Math.Exp(-juanjicolorRed[i,j]))*255);
+				juanjicolorGreen[i,j]=(int)((Math.Exp(juanjicolorGreen[i,j])-Math.Exp(-juanjicolorGreen[i,j]))/(Math.Exp(juanjicolorGreen[i,j])+Math.Exp(-juanjicolorGreen[i,j]))*255);
+				juanjicolorBlue[i,j]=(int)((Math.Exp(juanjicolorBlue[i,j])-Math.Exp(-juanjicolorBlue[i,j]))/(Math.Exp(juanjicolorBlue[i,j])+Math.Exp(-juanjicolorBlue[i,j]))*255);
+			}
+		}
+		void ELU(){
+			for(int i=0;i<width;++i)
+				for(int j=0;j<height;++j)
+			{
+				if(juanjicolorRed[i,j]<0)
+					juanjicolorRed[i,j]=(int)(r*(Math.Exp(juanjicolorRed[i,j])-1));
+				if(juanjicolorGreen[i,j]<0)
+					juanjicolorGreen[i,j]=(int)(r*(Math.Exp(juanjicolorGreen[i,j])-1));
+				if(juanjicolorBlue[i,j]<0)
+					juanjicolorBlue[i,j]=(int)(r*(Math.Exp(juanjicolorBlue[i,j])-1));
+			}
+		}
+		void juanji(int [,] b,out int [,] juanjicolor)
+		{
+
+			int [,] temp=new int[(width-juanjiwidth+1)/juanjibuchang+1,(height-juanjiwidth+1)/juanjibuchang+1];
+			for(int c=0;c<height-juanjiwidth+1;c+=juanjibuchang)
+			{
+				for(int t=0;t<width-juanjiwidth+1;t+=juanjibuchang)
+				{
+					int sum=0;
+					for(int i=0;i<juanjiwidth;++i)
+						for(int j=0;j<juanjiwidth;++j)
+					{
+						sum+=b[i+c,t+j]*juanjihe[i,j];
+					}
+					temp[c/juanjibuchang,t/juanjibuchang]=sum;	
+				}
+			}
+			juanjicolor=temp;
+		}
+		
+	}
+}
